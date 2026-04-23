2f5da11134878f81bef0ed246e6d7ea8a28be2c5
chayouleloup <hoze.chai@gmail.com>
2026-04-23 18:05:55 +0300

    Delete app/app directory

--- app/app/entreprise/import/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-import { getAuthUser } from '@/lib/supabase/server'
-import { ImportCapacitesClient } from '@/components/entreprise/ImportCapacitesClient'
-
-export default async function ImportCapacitesPage() {
-  const user = await getAuthUser()
-  if (!user) redirect('/auth/login')
-  return <ImportCapacitesClient company={user.company} />
-}
